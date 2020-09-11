5423d66c9c478e5adb1fce2f816aef30591db101
Administrator <Administrator@NOIDW10WFH6>
2020-09-12 00:25:10 +0530

    Initial Commit with one DTO

---
+++ myebill/src/test/java/com/api/myebill/MyebillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.myebill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyebillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
